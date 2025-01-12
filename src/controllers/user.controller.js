7afd17f0196d824c738ae7e96e5ba9bc0a9dfda5
ThunderCodes <thunderopbolte@gmail.com>
2025-01-12 14:57:33 +0530

    Fixed cover Image undefined issue

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,62 +1,70 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
-import {User} from "../models/user.model.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  const [fullName, username, email, password] = req.body;
+  const { fullname, username, email, password } = req.body;
 
   if (
-    [fullName, username, email, password].some((field) => {
+    [fullname, username, email, password].some((field) => {
       field?.trim() === "";
     })
   ) {
-    throw new ApiError(400, "All fields are required")
+    throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
-    $or:[{username},{email}]
-  })
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
 
   if (existedUser) {
-    throw new ApiError(409, "User Alredy exist")
+    throw new ApiError(409, "User Alredy exist");
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLoacalPath= req.files?.coverImage[0]?.path
+  //const coverImageLoacalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLoacalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLoacalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
-    throw new ApiError(400,"Avatar file required")
+    throw new ApiError(400, "Avatar file required");
   }
 
-  const avatar = await uploadOnCloudinary(avatarLocalPath)
-  const coverImage = await uploadOnCloudinary(coverImageLoacalPath)
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLoacalPath);
 
-  if(!avatar){
-    throw new ApiError(400,"Avatar Required")
+  if (!avatar) {
+    throw new ApiError(400, "Avatar Required");
   }
 
-  const user= await User.create({
-    fullName,
-    avatar:avatar.url,
-    coverImage:coverImage?.url || "",
+  const user = await User.create({
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
     email,
     password,
-    username:username.toLowerCase()
-  })
+    username: username.toLowerCase(),
+  });
 
   const createdUser = await User.findById(user._id).select(
     "-password -refreshToken"
   );
   if (!createdUser) {
-      throw new ApiError(500, "Something went wrong from our end")
+    throw new ApiError(500, "Something went wrong from our end");
   }
 
-  return res.status(201).json({
-    new ApiResponse(200, createdUser, "user registered")
-  })
-
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "user registered"));
 });
 
 export { registerUser };
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -13,7 +13,8 @@ const uploadOnCloudinary = async (localFilePath) => {
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    console.log("Successfull upload", response.url);
+    //console.log("Successfull upload", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath);
